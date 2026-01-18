6b1d177179d495c9987c9e4f0346f3b8080ad5f6
tlagu004.fiu <tlagu004.fiu@gmail.com>
2026-01-18 05:07:29 +0000

    Init Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import json
+import os
+
+st.set_page_config(page_title="Finance App", page_icon="💰", layout="wide")
+st.title("Hello")
