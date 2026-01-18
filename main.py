17827a4fe64a31c05413c459df663d81b5f8ae0b
tlagu004.fiu <tlagu004.fiu@gmail.com>
2026-01-18 06:03:43 +0000

    Implement File Uploader

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ import json
 import os
 
 st.set_page_config(page_title="Finance App", page_icon="💰", layout="wide")
-st.title("Hello")
+
+def load_transactions(file):
+    pass
+
+def main():
+    st.title("Financial Tracker")
+    uploaded_file = st.file_uploader("Upload transactions CSV File", type=["csv"])
+    if uploaded_file is not None: 
+        df = load_transactions(uploaded_file)
+
+main()
